813671aa428f07b9f737a992c94f3f906d10f0a3
Debojeet Dutta <debojeetdutta43@gmail.com>
2024-11-26 23:38:17 +0530

    es6 commited

---
+++ 07_ES6_Modules/app.js
@@ -0,0 +1,4 @@
+import num, {num2} from "./math.js";
+
+console.log(num);
+console.log(num2);
---
+++ 07_ES6_Modules/math.js
@@ -0,0 +1,5 @@
+
+// export const num = 3;
+const num = 18;
+export const num2 = 45;
+export default num;
