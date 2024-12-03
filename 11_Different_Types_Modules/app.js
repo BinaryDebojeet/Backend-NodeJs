5752e0f49727da789296d11f93a70b8d6a3ed5d3
Debojeet Dutta <debojeetdutta43@gmail.com>
2024-12-03 17:57:10 +0530

    types of modules

---
+++ 11_Different_Types_Modules/app.js
@@ -0,0 +1,18 @@
+// Native or Core Module
+// import fs from 'fs'
+import fs from 'node:fs'
+import net from 'node:net'
+import dgram from 'node:dgram'
+import http from 'node:http'
+import https from 'node:https'
+import child_process from 'node:child_process'
+import cluster from 'node:cluster'
+import zlib from 'node:zlib'
+import worker_threads from 'node:worker_threads'
+import crypto from 'node:crypto'
+
+console.log(fs)
+// User Module
+import {num} from './math.js'
+
+// Npm Modules
